52f053ff13bd6c50c12732483ec88218d200fa6b
Rooqx <ashfaga2005f@gmail.com>
2025-09-11 18:32:32 +0100

    Build sucess

--- dist/src/middleware/otp.middleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-//import rateLimit from "express-rate-limit";
-/*
-const otpRateLimiter = rateLimit({
-    windowMs: 5*60*1000 ,// 5 minutes
-    max: 5,
-
-})
-*/
--- dist/src/utils/sanitizer/sanitizeUser.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sanitizer = void 0;
-const sanitizer = (user) => {
-    const { password, ...safeUser } = user;
-    return safeUser;
-};
-exports.sanitizer = sanitizer;
